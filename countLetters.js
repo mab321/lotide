e7678db5edb8d460f14f001c7ecdb9e99e685124
Monther Balghonaim <monther.balghonaim@gmail.com>
2022-02-10 00:59:47 +0000

    Added CountLetters.js to count sentence letters

---
+++ countLetters.js
@@ -0,0 +1,32 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`💚💚💚 Assertion Passed: ${actual} === ${expected}`);
+
+  } else {
+    console.log(`🔴🔴🔴 Assertion Failed: ${actual} !==  ${expected}`);
+  }
+
+};
+const countLetters = function(sentence) {
+
+  const result = {};
+
+  for (const letter of sentence) {
+    if (letter !== " ") {
+      if (result[letter]) {
+        result[letter] += 1;
+      } else {
+        result[letter] = 1;
+      }
+    }
+  }
+  return result;
+ 
+};
+
+
+const letters = countLetters("lighthouse in the house");
+
+assertEqual(letters["h"],4);
+assertEqual(letters["e"],3);
+console.log(letters);
