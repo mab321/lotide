eedcddf51f85265afa718eab59c38fa1fd6c1e04
Monther Balghonaim <monther.balghonaim@gmail.com>
2022-02-15 02:03:07 +0000

    Added test file assertArraysEqualTest to hold test cases for assertArraysEqual

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,16 +1,5 @@
-const eqArrays = function(myArray1, myArray2) {
+const eqArrays = require('./eqArrays');
 
-  let comparison = true;
-
-  if(myArray1.length !== myArray2.length){
-    return comparison = false;
-  }
-
-  for (let i = 0; i < myArray1.length; i++) {
-    myArray1[i] === myArray2[i] ? comparison = true : comparison =false;
-  }
-  return comparison;
-}
 const assertArraysEqual = function(arrayList1, arrayList2) {
 
   if (Array.isArray(arrayList1) && Array.isArray(arrayList2)){
@@ -18,7 +7,7 @@ const assertArraysEqual = function(arrayList1, arrayList2) {
     eqArrays(arrayList1,arrayList2) === true ? console.log(`💚💚💚 Assertion Passed: ${arrayList1} === ${arrayList2}`) : console.log(`🔴🔴🔴 Assertion Failed: ${arrayList1} !==  ${arrayList2}`);
       
   }
-}
+};
 
-assertArraysEqual([1,2,3],[1,2,3]);
-assertArraysEqual([3,5],[1,2]);
+module.exports = assertArraysEqual;
+
