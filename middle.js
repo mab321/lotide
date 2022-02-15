211d3176f946fee45f71a8df4337ba76fdccc0fc
Monther Balghonaim <monther.balghonaim@gmail.com>
2022-02-15 02:14:54 +0000

    Added middle test file middleTest

--- middle.js
+++ middle.js
@@ -1,24 +1,5 @@
-const eqArrays = function(myArray1, myArray2) {
-
-  let comparison = true;
-
-  if(myArray1.length !== myArray2.length){
-    return comparison = false;
-  }
-
-  for (let i = 0; i < myArray1.length; i++) {
-    myArray1[i] === myArray2[i] ? comparison = true : comparison =false;
-  }
-  return comparison;
-}
-const assertArraysEqual = function(arrayList1, arrayList2) {
-
-  if (Array.isArray(arrayList1) && Array.isArray(arrayList2)){
-    
-    eqArrays(arrayList1,arrayList2) === true ? console.log(`💚💚💚 Assertion Passed: ${arrayList1} === ${arrayList2}`) : console.log(`🔴🔴🔴 Assertion Failed: ${arrayList1} !==  ${arrayList2}`);
-      
-  }
-}
+const eqArrays = require('./eqArrays');
+const assertArraysEqual = require('./assertArraysEqual');
 
 const middle = function(myArray) {
   let midArray = [];
@@ -39,6 +20,5 @@ const middle = function(myArray) {
 
 }
 
-assertArraysEqual(middle([1, 2, 3, 4]),[2,3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]),[3,4]);
-assertArraysEqual(middle([1]),[]);
+
+module.exports = middle;
