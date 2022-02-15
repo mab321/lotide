82b8f7b459caae630632623c8eee97013f989a44
Monther Balghonaim <monther.balghonaim@gmail.com>
2022-02-15 01:51:35 +0000

    Moved tail.js test cases to tailTest.js

---
+++ test/tailTest.js
@@ -0,0 +1,18 @@
+const assertEqual = require('../assertEqual');
+const tail = require('../tail');
+
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+const newWords = tail(words); // no need to capture the return value since we are not checking it
+const emptyWords = tail([]);
+
+//Test case 1 original array length unmodified and first element is still the same
+assertEqual(words.length, 3); // original array should still have 3 elements!
+assertEqual(words[0], "Yo Yo");
+
+// test case 2 check return tail array length and elements
+assertEqual(newWords.length,2); // returned array is 2 elements in length
+assertEqual(newWords[0], "Lighthouse"); // ensure first element is "Lighthouse"
+assertEqual(newWords[1], "Labs"); // ensure second element is "Labs"
+
+// test case 3 check empty array returned when empty array is passed to tail
+assertEqual(emptyWords.length,0); // empty array returns empty array length is 0
